5fb623928eb05edc69b8cba049e249b09496409b
chriskim2311 <chriskim2311@gmail.com>
2018-10-05 14:15:11 -0700

    Version 1.0-Chris Kim

--- oop/guess.js
+++ oop/guess.js
@@ -1,1 +1,47 @@
-guess.js
+$(document).ready(startApp);
+
+function startApp(){
+    GuessingGame;
+
+}
+
+
+
+class GuessingGame{
+    constructor() {
+        this.secretNumber;
+    }
+    guess_start() {
+        this.secretNumber = this.pickRandomNumber(1,10);
+        this.attachHandlers();
+    }
+    pickRandomNumber(min,max) {
+        return Math.floor( Math.random() * (max-min)) + min;
+    }
+    attachHandlers(){
+        $("#submitGuess").click( this.handleGuess.bind(this) );
+        $("#userGuess").focus( this.clearGuess.bind(this) );
+    }
+    handleGuess() {
+        this.userGuess = parseInt($("#userGuess").val());
+        if (this.userGuess < 1 || this.userGuess > 10) {
+            this.displayResult('invalid range.  Must be between 1 and 10');
+            return;
+        }
+
+        if (this.userGuess === this.secretNumber) {
+            this.displayResult('you got it!');
+        } else if (this.userGuess < this.secretNumber) {
+            this.displayResult('too low!');
+        } else if (this.userGuess > this.secretNumber) {
+            this.displayResult('too high!');
+        }
+    }
+    clearGuess() {
+            $("#userGuess").val('');
+    }
+    displayResult( message ){
+        $("#display").text( message );
+    }
+
+}
--- oop/main.js
+++ oop/main.js
@@ -1,7 +1,37 @@
-
-
 $(document).ready(startApp);
 
-function startApp(){
-	
+function startApp() {
+    var startGame = new GuessingGame
+    startGame.guess_start();
+}
+
+
+function repeatingWord(str) {
+    var lowercase = str.toLowerCase();
+    lowercase = lowercase.split(" ");
+    var array = [];
+    var outputObj = {};
+
+    for (vari = 0; i < lowercase.length; i++) {
+        outputObj.word = lowercase[i];
+
+        for (j = 0; j < outputObj.word.length; j++) {
+            if (outputObj.map[outputObj.word[j]]) {
+
+            }
+
+    else
+        {
+            outputObj.map = 1
+        }
+        array.push(outputObj);
+    }
+for (outputI =0; outputI < output.length; outputI++) {
+    for(key in array[outputI].map) {
+        if( [key] > highest) {
+            highestInd = outputI;
+        }
+    }
+}
+        return array[highestInd].word;
 }
